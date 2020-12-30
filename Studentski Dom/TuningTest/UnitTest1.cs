c4b584ffeea19742efec94b3fd9fa2f0ccd1860b
Dino Kopic <chuck_taylor97@live.com>
2020-12-30 21:01:09 +0100

    Evo bola

--- Studentski Dom/TuningTest/UnitTest1.cs
+++ Studentski Dom/TuningTest/UnitTest1.cs
@@ -21,5 +21,48 @@ namespace TuningTest
             Console.WriteLine("Kraj");
             // Zavrseno
         }
+
+        [TestMethod]
+        public void Test1()
+        {
+            StudentskiDom sd = new StudentskiDom(0);
+            Assert.AreEqual(0, sd.Sobe.Count);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            StudentskiDom sd = new StudentskiDom(1);
+            Soba s = sd.Sobe[0];
+            Assert.AreEqual(100, s.BrojSobe);
+            Assert.AreEqual(2, s.Kapacitet);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            StudentskiDom sd = new StudentskiDom(6);
+            Soba s = sd.Sobe[1];
+            Assert.AreEqual(101, s.BrojSobe);
+            Assert.AreEqual(2, s.Kapacitet);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            StudentskiDom sd = new StudentskiDom(3);
+            Soba s = sd.Sobe[1];
+            Assert.AreEqual(201, s.BrojSobe);
+            Assert.AreEqual(3, s.Kapacitet);
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            StudentskiDom sd = new StudentskiDom(3);
+            Soba s = sd.Sobe[2];
+            Assert.AreEqual(302, s.BrojSobe);
+            Assert.AreEqual(4, s.Kapacitet);
+        }
     }
 }
